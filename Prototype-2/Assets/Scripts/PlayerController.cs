6bb33e99866b2294a45e0d3bdef3f5489072c260
seanpride <keseanpride12@gmail.com>
2022-02-27 23:07:10 -0600

    Updated player boundary movement
    "PlayerController" script now limits player movement along the x-axis by checking if its x-axis range variable is less than or greater than its current transform position along the axis.

--- Prototype-2/Assets/Scripts/PlayerController.cs
+++ Prototype-2/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     // variables
     public float horizontalInput;
     public float horizontalSpeed = 10.0f;
+    public float xRange = 15.0f;
     
     void Start()
     {
@@ -21,8 +22,12 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * horizontalSpeed);
 
         // boundary movement set for player along x-axis
-        if (transform.position.x < -10) {
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        if (transform.position.x < -xRange) {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
     }
 }
