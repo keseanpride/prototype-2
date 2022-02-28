ecc3f7ddf2599a026086995c7a6cd441801f6f27
seanpride <keseanpride12@gmail.com>
2022-02-27 23:33:34 -0600

    Prefabs folder added to assets folder
    Assets folder has an additional folder of "Prefabs" within Unity editor. Along with "PlayerController" script now updated for player to have a public reference to a food GameObject prefab.

--- Prototype-2/Assets/Scripts/PlayerController.cs
+++ Prototype-2/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float horizontalSpeed = 10.0f;
     public float xRange = 15.0f;
+    public GameObject projectilePrefab;
     
     void Start()
     {
