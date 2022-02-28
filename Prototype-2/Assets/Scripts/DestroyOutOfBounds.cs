2e5926d1e5ba49a556af0aa2fd377af2fbb6f0c9
seanpride <keseanpride12@gmail.com>
2022-02-28 13:56:49 -0600

    Updated GameObject deletion
    "DestroyOutOfBounds" script has now been applied to all animal prefab GameObjects to prevent over-duplication of themselves within SceneView.

--- Prototype-2/Assets/Scripts/DestroyOutOfBounds.cs
+++ Prototype-2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -6,6 +6,7 @@ public class DestroyOutOfBounds : MonoBehaviour
 {
     // variables
     private float topBound = 30.0f;
+    private float lowerBound = -10.0f;
 
     void Start()
     {
@@ -14,8 +15,10 @@ public class DestroyOutOfBounds : MonoBehaviour
 
     void Update()
     {
-        // destroys gameobject if its transform position on the z-axis exceeds more than its top boundary
+        // destroys gameobject if its transform position on the z-axis exceeds more than its top or lower boundary
         if (transform.position.z > topBound) {
+            Destroy(gameObject);
+        }else if (transform.position.z < lowerBound) {
             Destroy(gameObject);
         }
     }
