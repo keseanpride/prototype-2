ae98e51de209cefdcea6c3c6a7b4340514e85a4f
seanpride <keseanpride12@gmail.com>
2022-02-27 22:55:03 -0600

    Implemented player boundary movement
    Player has limited movement along the x-axis via "PlayerController" script to avoid running offscreen during gameplay.

--- Prototype-2/Assets/Scripts/PlayerController.cs
+++ Prototype-2/Assets/Scripts/PlayerController.cs
@@ -16,8 +16,13 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // player's horizontal input is set from Unity's input manager
-        horizontalInput = Input.GetAxis("horizontal");
+        horizontalInput = Input.GetAxis("Horizontal");
         // player's horizontal movement
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * horizontalSpeed);
+
+        // boundary movement set for player along x-axis
+        if (transform.position.x < -10) {
+            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        }
     }
 }
