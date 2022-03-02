759d1dce2ddc5894ca3da19208ddb4461a3f4564
seanpride <keseanpride12@gmail.com>
2022-03-02 09:29:30 -0600

    Implemented spawn manager system
    SceneView has a newly added GameObject of "SpawnManger". Along with a new C# script of "SpawnManager" attached to it for managing prefab spawning within the scene.

---
+++ Prototype-2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    // variables
+    public GameObject[] animalPrefabs;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
