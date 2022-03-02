92ea8a89779b463ac46bfc601762017a95c5c8a0
seanpride <keseanpride12@gmail.com>
2022-03-02 10:21:08 -0600

    Cleaned up code hierarchy
    Additional variables of "spawnRangX", spawnPosZ, & spawnPos were added in place of former syntax of animal prefab spawning.

--- Prototype-2/Assets/Scripts/SpawnManager.cs
+++ Prototype-2/Assets/Scripts/SpawnManager.cs
@@ -6,6 +6,8 @@ public class SpawnManager : MonoBehaviour
 {
     // variables
     public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20.0f;
+    private float spawnPosZ = 20.0f;
 
     void Start()
     {
@@ -16,11 +18,12 @@ public class SpawnManager : MonoBehaviour
     {
         // variables
         int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
 
         // player input for spawning animal prefabs
         if (Input.GetKeyDown(KeyCode.S)) {
             // spawns a random animal prefab from given array of animal prefabs along with its preset rotation
-            Instantiate(animalPrefabs[animalIndex], new Vector3(Random.Range(-20, 20), 0, 20), animalPrefabs[animalIndex].transform.rotation);
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
         }
     }
 }
