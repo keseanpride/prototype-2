85af6c1f1ef513ca3ab35f65a9cdc9d01145e621
seanpride <keseanpride12@gmail.com>
2022-03-06 23:31:27 -0600

    Updated spawn manager system
    A new function of "SpawnRandomAnimal" has been added in place of instantiating a random animal prefab directly from player's input. Now once player presses the "S" key on the keyboard, the function is called in place to yield the same result of instantiating a new animal prefab within the scene.

--- Prototype-2/Assets/Scripts/SpawnManager.cs
+++ Prototype-2/Assets/Scripts/SpawnManager.cs
@@ -16,14 +16,19 @@ public class SpawnManager : MonoBehaviour
 
     void Update()
     {
+        // player input for spawning animal prefabs from animal spawning function
+        if (Input.GetKeyDown(KeyCode.S)) {
+            // calls animal spawning function
+            SpawnRandomAnimal();
+        }
+    }
+
+    void SpawnRandomAnimal() {
         // variables
         int animalIndex = Random.Range(0, animalPrefabs.Length);
         Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
 
-        // player input for spawning animal prefabs
-        if (Input.GetKeyDown(KeyCode.S)) {
-            // spawns a random animal prefab from given array of animal prefabs along with its preset rotation
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
-        }
+        // spawns a random animal prefab from given array of animal prefabs along with its preset rotation
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
